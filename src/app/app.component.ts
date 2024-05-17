ef9b04dd4a733f190588ea9be59c0a309352f346
Richard Burman <richard.burman@harperjames.co.uk>
2024-05-17 16:26:11 +0100

    Connect account.services and logging.services for more effective code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   accounts: {name: string, status: string} [] = [];
 
   constructor(private accountsService: AccountsService) {}
